7ccc012ef83327118dd92b804e0036301775b2d6
SuperLandy <zzq0226@hotmail.com>
2018-12-04 14:20:58 +0800

    增加端口扫描工具

---
+++ Other/scan_port.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+
+import socket
+socket.setdefaulttimeout(0.1)   #设置socker超时，单位秒
+def get_port_status(ip,port):
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #SOCK_DGRAM是udp协议,STREAM是tcp协议
+    try:
+        server.connect((ip,port))
+        print('ip：{}的{}端口可用'.format(ip,port))
+    except Exception as e:
+        print(server.getsockname()[0])
+        pass
+    finally:
+        server.close()
+
+if __name__ == '__main__':
+    ip = '120.78.87.162'
+    for port in range(20,90):
+        get_port_status(ip,port)
