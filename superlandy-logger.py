566e087e73246c085284b5e32682e9c8d04a7177
SuperLandy <44797172+SuperLandy@users.noreply.github.com>
2021-11-29 14:32:05 +0800

    Create superlandy-logger.py

---
+++ superlandy-logger.py
@@ -0,0 +1,31 @@
+import logging
+from logging import handlers
+
+
+class Logger(object):
+    level_relations = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'crit': logging.CRITICAL
+    }
+    # when 按日进行文件归档
+    # backCount 保留历史文件数
+    def __init__(self, filename, level='info', when='D', backCount=3,
+                 fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)
+        self.logger.setLevel(self.level_relations.get(level))
+        sh = logging.StreamHandler()
+        sh.setFormatter(format_str)
+        th = handlers.TimedRotatingFileHandler(filename=filename, when=when, backupCount=backCount, encoding='utf-8')
+        th.setFormatter(format_str)
+        self.logger.addHandler(sh)
+        self.logger.addHandler(th)
+
+
+if __name__ == '__main__':
+    log_path = 'TlwAILog.log'  # log path
+    log = Logger(log_path, level='debug', backCount=5)
+    log.logger.info('wocao')
